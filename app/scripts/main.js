baa3833f073d94cd8709972df3d2efefd847f47c
Annie <anne.bourgeois1@gmail.com>
2015-03-19 21:11:33 -0400

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  console.log('\'allo!');
+  // $( "div#tech" ).hide();
+  // $( "div#lang" ).hide();
+  // $( "div#proj" ).hide();
+
+  // $( 'button#tech' ).click(function () {
+  //   $( "div#proj" ).hide();
+  //   $( "div#lang" ).hide();
+
+  //   if ( $( "div#tech" ).is( ":hidden" ) ) {
+  //     $( "div#tech" ).slideDown( "slow" );
+  //   } else {
+  //     $( "div#tech" ).hide();
+  //   }
+  // });
+
+  // $( 'button#lang' ).click(function () {
+  //   $( "div#tech" ).hide();
+  //   $( "div#proj" ).hide();
+
+  //   if ( $( "div#lang" ).is( ":hidden" ) ) {
+  //     $( "div#lang" ).slideDown( "slow" );
+  //   } else {
+  //     $( "div#lang" ).hide();
+  //   }
+  // });
+
+  // $( 'button#proj' ).click(function () {
+  //   $( "div#tech" ).hide();
+  //   $( "div#lang" ).hide();
+  //   if ( $( "div#proj" ).is( ":hidden" ) ) {
+  //     $( "div#proj" ).slideDown( "slow" );
+  //   } else {
+  //     $( "div#proj" ).hide();
+  //   }
+  // });
+});
